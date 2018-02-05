afc65c8f244285f6dfd23f9c55f37ce890c32da2
Alena Bukilić <bukilicalena@gmail.com>
2018-02-05 20:46:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,114 +1,128 @@
 const main = document.querySelector('#main');
 let divArr = [];
 
-myData.forEach((element) => {
+window.onload = displayUsers;
 
-	const personBox = document.createElement('div');
-	personBox.innerText = element['firstName'] + ' ' + element['surname'];
-	personBox.classList.add('name');
-	if(element['age'] != null) {
+// display all users with personal info (age, gender)
 
-		const personBoxInfo = document.createElement('p');
-		personBoxInfo.innerText = element['age'] + ' ' + element['gender'];
-		personBoxInfo.classList.add('info');
-		personBox.appendChild(personBoxInfo);
-	}
-	else {
-		const personBoxInfo = document.createElement('p');
-		personBoxInfo.innerText = element['gender'];
-		personBoxInfo.classList.add('info');
-		personBox.appendChild(personBoxInfo);	
-	}
+function displayUsers() {
 
-	main.appendChild(personBox);
-	divArr.push(personBox);
+	myData.forEach((element) => {
 
-	const dataDiv = document.createElement('div');
-	dataDiv.classList.add('dataMain');
-	dataDiv.classList.add('hidden');
-	personBox.appendChild(dataDiv);
-	const directFriends = document.createElement('div');
-	const firendsOfFriends = document.createElement('div');
-	const suggestedFriends =document.createElement('div');
-	directFriends.classList.add('dataChildren');
-	firendsOfFriends.classList.add('dataChildren');
-	suggestedFriends.classList.add('dataChildren');
-	dataDiv.appendChild(directFriends);
-	dataDiv.appendChild(firendsOfFriends);
-	dataDiv.appendChild(suggestedFriends);
-	directFriends.innerText = 'Direct Friends:';
-	firendsOfFriends.innerText = 'Firends Of Friends:';
-	suggestedFriends.innerText = 'Suggested Friends:';
+		const personBox = document.createElement('div');
+		personBox.innerText = element['firstName'] + ' ' + element['surname'];
+		personBox.classList.add('name');
+		if(element['age'] != null) {
 
-	for(j = 0; j < element['friends'].length; j++){
-		const directFriend = document.createElement('p');
-		directFriend.classList.add('friendNames');
-		const friend = element['friends'][j];
-		directFriend.innerText = myData[friend - 1]['firstName'] + ' ' + myData[friend - 1]['surname'];
-		directFriends.appendChild(directFriend);
-		for(k = 0; k < myData[friend-1]['friends'].length; k++){
-			if(element['id'] != myData[friend-1]['friends'][k]){
-				const friendFriend = document.createElement('p');
-				friendFriend.classList.add('friendNames');
-				const friendOf = myData[friend-1]['friends'][k];
-				friendFriend.innerText = myData[friendOf - 1]['firstName'] + ' ' + myData[friendOf - 1]['surname'];
-				firendsOfFriends.appendChild(friendFriend);
-			}
+			const personBoxInfo = document.createElement('p');
+			personBoxInfo.innerText = element['age'] + ' ' + element['gender'];
+			personBoxInfo.classList.add('info');
+			personBox.appendChild(personBoxInfo);
 		}
-	}
+		else {
+			const personBoxInfo = document.createElement('p');
+			personBoxInfo.innerText = element['gender'];
+			personBoxInfo.classList.add('info');
+			personBox.appendChild(personBoxInfo);	
+		}
 
-	let bool = false;
-	if(element['friends'].length > 1) {
-		for(x = 0; x < myData.length; x++){
-			if(myData[x]['id'] != element['id']){
-				if(findMatches(myData[x]['friends'], element['friends'])){
-					const suggestedFriend = document.createElement('p');
-					suggestedFriend.classList.add('friendNames');
-					suggestedFriend.innerText = myData[x]['firstName'] + ' ' + myData[x]['surname'];	
-					suggestedFriends.appendChild(suggestedFriend);
-					bool = true;
+		main.appendChild(personBox);
+		divArr.push(personBox);
+
+		// friends info data, currently hidden
+		const dataDiv = document.createElement('div');
+		dataDiv.classList.add('dataMain');
+		dataDiv.classList.add('hidden');
+		personBox.appendChild(dataDiv);
+		const directFriends = document.createElement('div');
+		const firendsOfFriends = document.createElement('div');
+		const suggestedFriends =document.createElement('div');
+		directFriends.classList.add('dataChildren');
+		firendsOfFriends.classList.add('dataChildren');
+		suggestedFriends.classList.add('dataChildren');
+		dataDiv.appendChild(directFriends);
+		dataDiv.appendChild(firendsOfFriends);
+		dataDiv.appendChild(suggestedFriends);
+		directFriends.innerText = 'Direct Friends:';
+		firendsOfFriends.innerText = 'Firends Of Friends:';
+		suggestedFriends.innerText = 'Suggested Friends:';
+
+		// for each user display list of friends and friends of friends
+		for(j = 0; j < element['friends'].length; j++){
+			const directFriend = document.createElement('p');
+			directFriend.classList.add('friendNames');
+			const friend = element['friends'][j];
+			directFriend.innerText = myData[friend - 1]['firstName'] + ' ' + myData[friend - 1]['surname'];
+			directFriends.appendChild(directFriend);
+			for(k = 0; k < myData[friend-1]['friends'].length; k++){
+				if(element['id'] != myData[friend-1]['friends'][k]){
+					const friendFriend = document.createElement('p');
+					friendFriend.classList.add('friendNames');
+					const friendOf = myData[friend-1]['friends'][k];
+					friendFriend.innerText = myData[friendOf - 1]['firstName'] + ' ' + myData[friendOf - 1]['surname'];
+					firendsOfFriends.appendChild(friendFriend);
 				}
 			}
 		}
-		if(bool == false){
+
+		let bool = false;
+
+		if(element['friends'].length > 1) {
+
+			// for each user display list of suggested friends, if not disiplay 'none'
+			for(x = 0; x < myData.length; x++){
+				if(myData[x]['id'] != element['id']){
+					if(findMatches(myData[x]['friends'], element['friends'])){
+						const suggestedFriend = document.createElement('p');
+						suggestedFriend.classList.add('friendNames');
+						suggestedFriend.innerText = myData[x]['firstName'] + ' ' + myData[x]['surname'];	
+						suggestedFriends.appendChild(suggestedFriend);
+						bool = true;
+					}
+				}
+			}
+			if(bool == false){
+				const suggestedFriend = document.createElement('p');
+				suggestedFriend.classList.add('friendNames');
+				suggestedFriend.innerText = 'None';	
+				suggestedFriends.appendChild(suggestedFriend);	
+			}
+			bool = false;	
+		}
+		else {
 			const suggestedFriend = document.createElement('p');
 			suggestedFriend.classList.add('friendNames');
 			suggestedFriend.innerText = 'None';	
 			suggestedFriends.appendChild(suggestedFriend);	
 		}
-		bool = false;	
-	}
-	else {
-		const suggestedFriend = document.createElement('p');
-		suggestedFriend.classList.add('friendNames');
-		suggestedFriend.innerText = 'None';	
-		suggestedFriends.appendChild(suggestedFriend);	
-	}
 
 
-	
-});
+		
+	});
 
+	// display friends info on one particular user
+	divArr.forEach(element => element.addEventListener('click',() => {
 
-divArr.forEach(element => element.addEventListener('click',() => {
+		divArr.forEach((element) => {
 
-	divArr.forEach((element) => {
+			if(element.classList == 'big'){
+				element.classList.remove('big');
+				element.classList.add('name');
+				element.lastChild.classList.add('hidden');
+			}
+		});
 
-		if(element.classList == 'big'){
-			element.classList.remove('big');
-			element.classList.add('name');
-			element.lastChild.classList.add('hidden');
+		if(element.classList == 'name'){
+			element.classList.remove('name');
+			element.classList.add('big');
+			element.lastChild.classList.remove('hidden');
 		}
-	})
 
-	if(element.classList == 'name'){
-		element.classList.remove('name');
-		element.classList.add('big');
-		element.lastChild.classList.remove('hidden');
-	}
+	}));
 
-}));
+}
 
+// check if 2 or more elements in the array match
 function findMatches(arr1, arr2){
 
 	let helpArr = [];
